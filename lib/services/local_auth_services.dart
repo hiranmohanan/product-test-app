1006dee8bdb03b7c5ecf8ef66570c692b5107183
hiranmohanan <hiran9446597824@gmail.com>
2024-05-29 10:07:42 +0530

    run

--- lib/services/local_auth_services.dart
+++ lib/services/local_auth_services.dart
@@ -1,47 +1,41 @@
-import 'package:local_auth/local_auth.dart';
-
 final LocalAuthentication localAuth = LocalAuthentication();
-final List<BiometricType> availableBiometrics = [
+final List<BiometricType> availableBiometrics = [];
 
-];
-class BioMatrics{
-
-
-Future<bool> authenticateBio() async {
-  bool authenticated = false;
-  try {
-    if (await hasBioMetrics()) {
-      authenticated = await localAuth.authenticate(
-        localizedReason: 'Authenticate to access the app',
-        options: AuthenticationOptions(
-         biometricOnly: true,
-        ),
-      );
+class BioMatrics {
+  Future<bool> authenticateBio() async {
+    bool authenticated = false;
+    try {
+      if (await hasBioMetrics()) {
+        authenticated = await localAuth.authenticate(
+          localizedReason: 'Authenticate to access the app',
+          options: AuthenticationOptions(
+            biometricOnly: true,
+          ),
+        );
+      }
+    } catch (e) {
+      print(e);
     }
-   
-  } catch (e) {
-    print(e);
+    return authenticated;
   }
-  return authenticated;
-}
 
-Future<bool> hasBioMetrics() async {
-  bool hasBioMetrics = false;
-  try {
-    hasBioMetrics = await localAuth.canCheckBiometrics; 
-  } catch (e) {
-    print(e);
+  Future<bool> hasBioMetrics() async {
+    bool hasBioMetrics = false;
+    try {
+      hasBioMetrics = await localAuth.canCheckBiometrics;
+    } catch (e) {
+      print(e);
+    }
+    return hasBioMetrics;
   }
-  return hasBioMetrics;
-}
 
-Future<List<BiometricType>> getAvailableBiometrics() async {
-  List<BiometricType> availableBiometrics = [];
-  try {
-    availableBiometrics = await localAuth.getAvailableBiometrics();
-  } catch (e) {
-    print(e);
+  Future<List<BiometricType>> getAvailableBiometrics() async {
+    List<BiometricType> availableBiometrics = [];
+    try {
+      availableBiometrics = await localAuth.getAvailableBiometrics();
+    } catch (e) {
+      print(e);
+    }
+    return availableBiometrics;
   }
-  return availableBiometrics;
-}
 }
